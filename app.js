3fac6fdfe30203e1d127728d66fb8793fafd09e8
Youssef-C <youssef.chardi@student.kyh.se>
2021-05-06 15:04:52 +0200

    Still on episode 6 tutorial

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ const express = require('express');
 const app = express();
 
 //request listener
-app.listen(3000);   
+app.listen(/*This number refers to the port being listened to*/3000);   
 
 app.get('/', (req, res) => {
     
     res.send("<p>Home Page</p>");
+    console.log("a request was answered");
+
+});
+
+app.get('/about', (req, res) => {
+    
+    res.send("<p>About Page</p>");
+    console.log("a request for About page was answered");
 
 });
