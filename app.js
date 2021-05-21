e2bc51be9a8f518e195dd35d3842b694ed331550
Youssef-C <youssef.chardi@student.kyh.se>
2021-05-21 11:45:29 +0200

    Now on episode 7

--- app.js
+++ app.js
@@ -7,15 +7,22 @@ const app = express();
 app.listen(/*This number refers to the port being listened to*/3000);   
 
 app.get('/', (req, res) => {
-    
-    res.send("<p>Home Page</p>");
-    console.log("a request was answered");
-
+    res.sendFile('./public/index.html', { root: __dirname });
+    console.log("a request for Home page was answered");
 });
 
 app.get('/about', (req, res) => {
-    
-    res.send("<p>About Page</p>");
+    res.sendFile('./public/about.html', { root: __dirname });
     console.log("a request for About page was answered");
-
 });
+
+//Redirects
+app.get('/about-us', (req, res) => {
+res.redirect('/about');
+})
+
+// 404 Page - Must be at the bottom of page!
+app.use((req, res) => {
+    res.status(404).sendFile('./public/404.html', { root: __dirname });
+    console.log("Error met and responded too");
+})
