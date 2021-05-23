4825de422c9ded799272091edfcb3bbcb42ac47b
Youssef-C <youssef.chardi@student.kyh.se>
2021-05-23 12:33:59 +0200

    Episode 10, I have set up database and blog display

--- app.js
+++ app.js
@@ -1,35 +1,48 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 //Express App
 const app = express();
+
+//Connect to MongoDB
+const dbURI = 'mongodb+srv://youssef_test:youssefTest@cluster0.xwazt.mongodb.net/node-tuts?retryWrites=true&w=majority';
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true })
+    .then(()=> app.listen(/*This number refers to the port being listened to*/3000))
+    .then(() => console.log('connected to database'))
+    .catch((err) => { 
+        console.log(err)
+    });
 
 //Register view engine
 app.set('view engine', 'ejs');
 app.set('views', 'public');
 
-//request listener
-app.listen(/*This number refers to the port being listened to*/3000);   
-
-
 //Middleware and static files
 app.use(express.static('public'));
 app.use(morgan('dev')); 
 
+//Routes
 app.get('/', (req, res) => {
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet:'Lorem ipsum'},
-        {title: 'Mario finds start', snippet:'Lorem ipsum'},
-        {title: 'How to defeat bowser', snippet:'Lorem ipsum'}
-    ];
-    res.render('index', { title: 'Home', blogs });
-    console.log("request for home page answered")
+    res.redirect('/blogs')
 });
 
 app.get('/about', (req, res) => {
     res.render('about', { title: 'About'});
     console.log("a request for About page was answered");
 });
+
+// blog Routes
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({createdAt: -1 })
+        .then((result) => {
+            res.render('index', {title: 'All blogs', blogs: result })
+        })
+        .catch((err) => {
+            console.log(err)
+        });
+})
 
 app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'Create'});
